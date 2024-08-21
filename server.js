f53d3f5f333b6aba52d21cf08d1ce95a1f89a038
lehonganh29122002 <lehonganh29122002@gmail.com>
2024-08-21 16:24:36 +0700

    add .env ejs gitignore

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const { createServer } = require("node:http");
-const hostname = "127.0.0.1";
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const express = require("express"); //common js
+const app = express(); //app express
+//import dotenv để dùng process.env
+require("dotenv").config();
+// const port = 3000; //port ==> hardcode
+const port = process.env.PORT || 8080;
+const path = require("path");
+const hostname = process.env.HOST_NAME;
+// console.log(process.env); // remove this after you've confirmed it is working
+
+//config template ejs (Khai bao truoc route)
+app.set("views", path.join(__dirname, "./src/views"));
+app.set("view engine", "ejs");
+//khai bao route
+//res, req là 2 biến môi trường cho sẵn chỉ cần dùng
+app.get("/", (req, res) => {
+  res.send("Express says hello world!");
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+//more routes
+app.get("/honganh", (req, res) => {
+  res.send("Express says honganh!");
+});
+app.get("/leanh", (req, res) => {
+  //   res.send("<h1>Express says leanh</h1>!");
+  res.render("sample.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on hostname and port ${hostname}:${port}`);
 });
